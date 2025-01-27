776d408a899e7e0e1d3d891a4fe73bdc5bb2ac44
Vikas A Adodariya <106027283+VIKASADODARIYA@users.noreply.github.com>
2025-01-27 12:41:15 +0530

    Create 162_FInd Peak Element.py

---
+++ 162_FInd Peak Element.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid
+        
+        return left
+
+nums = [2, 1]
+result = Solution().findPeakElement(nums)
+print(result)
